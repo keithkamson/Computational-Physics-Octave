4f93ab79fc278b6a992b18341f33198e1cc6fc7a
Keith Kamson Fernandes <85413860+keithkamson@users.noreply.github.com>
2023-04-15 13:43:31 +0530

    update experiment 6

--- Experiment 6/expt_6.m
+++ Experiment 6/expt_6.m
@@ -22,7 +22,7 @@ endfor
 prob1 = count1/N(1);
 pi1 = 4*prob1;
 
-#MSG
+#MID-SQUARE METHO
 count2 = 0;
 for i=1:N(1),
   if (((msg(i))^2 + (rand())^2)^0.5 < 1)
@@ -33,7 +33,7 @@ endfor
 prob2 = count2/N(1);
 pi2 = 4*prob2;
 
-#OCTAVE
+#OCTAVE RAND() METHOD
 count3 = 0;
 for i=1:N(1),
   if (((oct(i))^2 + (rand())^2)^0.5 < 1)
@@ -44,6 +44,32 @@ endfor
 prob3 = count3/N(1);
 pi3 = 4*prob3;
 
+#Pi Values
 pi1
 pi2
 pi3
+
+%Error Calculation
+error1 = ((pi1/3.1415)*100)-100;
+error2 = ((pi2/3.1415)*100)-100;
+error3 = ((pi3/3.1415)*100)-100;
+
+printf("\n")
+
+error1
+error2
+error3
+printf("\n")
+
+#LCG METHOD
+printf ("a. LCG METHOD \n Pi Value Calculated using LCG Method = %d \n Error(LCG Method) = %d%%",pi1,error1)
+printf("\n")
+printf("\n")
+
+#MID-SQUARE METHOD
+printf ("b. MID-SQUARE METHOD \n Pi Value Calculated using Mid-Square Method = %d \n Error(Mid-Square Method) = %d%%",pi2,error2)
+printf("\n")
+printf("\n")
+
+#OCTAVE RAND() METHOD
+printf ("c. OCTAVE \n Pi Value Calculated using Octave Rand() = %d \n Error(Octave Rand()) = %d%%",pi3,error3)
