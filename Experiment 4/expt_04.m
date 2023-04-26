15029c13a25e87237effb5ebfdcd3ac00b8214ba
Keith Kamson Fernandes <keithkamson@gmail.com>
2023-04-26 09:39:33 +0530

    update comments

--- Experiment 4/expt_04.m
+++ Experiment 4/expt_04.m
@@ -3,14 +3,18 @@
 #Simple Pendulum Simulation
 
 theta = pi/4
-thetd = 0
+omega = 0
 l = 1
 dt = 0.01
 
+#theta = angle
+#omega = angular velocity
+#alpha = angular acceleration
+
 for i=[0:1:10000],
-    thetdd = -9.8*sin(theta)/l;
-    thetd = thetd+thetdd*dt;
-    theta = theta+thetd*dt;
+    alpha = -9.8*sin(theta)/l;
+    omega = omega+alpha*dt;
+    theta = theta+theomegatd*dt;
     plot(l*sin(theta), l*(1-cos(theta)), 'ko', "markersize", 10,[0,l*sin(theta)], [1, l*(1-cos(theta))], "linewidth", 1.5);
     axis("square");
     xlim([-l-0.1, l+0.1]);
